97f9ba82b699528e276a432201fc0fb7084f5454
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-09-30 20:07:02 +0100

    Added SQLFluff for formatting

--- WideWorldImportersDevelopment/Tests/StoredProcedures/Test_Website_Customers_Exists.sql
+++ WideWorldImportersDevelopment/Tests/StoredProcedures/Test_Website_Customers_Exists.sql
@@ -1,11 +1,10 @@
 CREATE
+OR
 
-
-PROCEDURE Tests.Test_Website_Customers_Exists
+ALTER PROCEDURE Tests.Test_Website_Customers_Exists
 AS
 BEGIN
     -- Assert that the object exists in the database
     EXEC TSQLt.AssertObjectExists 'Website.Customers'
 END;
 GO
-
