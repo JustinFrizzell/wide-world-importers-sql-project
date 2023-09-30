9f1d807493f90734d6232cb00a0544d791f38dfd
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-09-30 19:57:10 +0100

    Ignore tSQLt

---
+++ WideWorldImportersDevelopment/Security/Tests.sql
@@ -0,0 +1,4 @@
+CREATE SCHEMA [Tests]
+    AUTHORIZATION [dbo];
+GO
+
---
+++ WideWorldImportersDevelopment/Tests/StoredProcedures/Test_Website_Customers_Exists.sql
@@ -0,0 +1,11 @@
+CREATE
+
+
+PROCEDURE Tests.Test_Website_Customers_Exists
+AS
+BEGIN
+    -- Assert that the object exists in the database
+    EXEC TSQLt.AssertObjectExists 'Website.Customers'
+END;
+GO
+
