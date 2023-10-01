3006af68193a757a036948fc688bbfc0a1abb15f
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-10-01 10:55:49 +0100

    Added utilities script

---
+++ Utilities.sql
@@ -0,0 +1,17 @@
+-- Run all tSQLt tests
+EXEC tSQLt.RunAll;
+
+-- Column descriptions
+SELECT 
+    OBJECT_NAME(ep.major_id) AS ObjectName
+    , c.name AS ColumnName
+    , ep.name AS PropertyName
+    , ep.value AS PropertyValue
+FROM 
+    sys.extended_properties AS ep
+INNER JOIN 
+    sys.columns AS c
+    ON ep.major_id = c.object_id AND ep.minor_id = c.column_id
+WHERE 
+    ep.name = 'Description'
+ORDER BY ObjectName
